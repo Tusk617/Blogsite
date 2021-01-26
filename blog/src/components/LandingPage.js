94bae546de06ea613313d233d96920b6dc813c31
Tusk617 <59673124+Tusk617@users.noreply.github.com>
2021-01-26 16:10:03 -0500

    Added some more styling, centering and defining space for the elements

--- blog/src/components/LandingPage.js
+++ blog/src/components/LandingPage.js
@@ -1,12 +1,15 @@
 import pfp from "./media/default_pfp.jpg";
+import "./LandingPage.css";
 
 export const Landing = () => {
 
 
     return (
         <div class="LandingPage">
-            <img src={pfp} alt="Placeholder profile headshot" />
-            <h1>Placeholder Title</h1>
+            <div class="TitleBar">
+                <img id="headshot" src={pfp} alt="Placeholder profile headshot" />
+                <h1 id="title">Placeholder Title</h1>
+            </div>
             <div class="AboutSection">
                 <h2>About</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc molestie, dolor at imperdiet viverra, odio nibh convallis tellus, a consequat arcu sapien in tellus. Suspendisse potenti. Nulla placerat purus non erat vehicula iaculis. Pellentesque fringilla maximus hendrerit. Phasellus tortor nisi, accumsan ornare massa vel, efficitur dictum felis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque purus erat, sagittis eu pellentesque vitae, pretium sit amet diam.</p>
